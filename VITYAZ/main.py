9ae9f07fb940a90f65ba3cd40a47f78be0836090
UberPipa <nu.takoe.b@gmail.com>
2022-12-16 12:48:58 +0300

    start

---
+++ VITYAZ/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from pandas import read_csv
+
+# with open('test.csv', encoding='utf-8') as array:
+#     for line in array:
+#         print(line.strip())
+
+array = read_csv("test.csv")
+
+
+
+print(array)
